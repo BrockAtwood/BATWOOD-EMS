6e5b014130558aa7cb2320874b10aee99e2becd0
Brock Atwood <brock.atwood@du.edu>
2021-09-01 21:58:42 -0600

    completed schema file from directions and activity examples

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+/* basic drop, create, & use boiler for schema*/
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+/* examples from activity #22 */
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+/* Decimal from sql tutorial . net (p,s) p = total place values, s = place values to the right of the decimal*/
+/* Foreign and reference lines from activity #20 */
+CREATE TABLE role (
+    id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES manager(id)
+);
